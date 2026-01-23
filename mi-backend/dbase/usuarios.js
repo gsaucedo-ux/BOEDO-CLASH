b19016a1171a1644910d7eaaa4d98b58ea1437d0
gsaucedo-ux <gsaucedo@fi.uba.ar>
2026-01-23 23:47:50 +0000

    agrego endpoint de crear usuario

--- mi-backend/dbase/usuarios.js
+++ mi-backend/dbase/usuarios.js
@@ -16,7 +16,21 @@ async function getusuario(id) {
         const respuesta=await dbclient.query('SELECT * FROM usuario WHERE id=$1', [id]);
         return respuesta.rows[0];
 };
-
+async function create_usuario(nombre, correo, contraseña, alias, carta_favorita){
+        const respuesta=await dbclient.query('INSERT INTO usuario (nombre, correo, contraseña, alias, carta_favorita) VALUES ($1, $2, $3, $4, $5) RETURNING *', [nombre, correo, contraseña, alias, carta_favorita]);
+        if (respuesta.rowCount===0){
+                return undefined;
+        }
+        return respuesta.rows[0];
+}
+async function verificacion_correo(correo) {
+        const respuesta=await dbclient.query('SELECT * FROM usuario WHERE correo=$1', [correo]);
+        return respuesta;
+}
+async function alias_usuario(alias) {
+        const respuesta=await dbclient.query('SELECT * FROM usuario WHERE alias=$1', [alias]);
+        return respuesta;
+}
 module.exports={
-        getallusuarios,getusuario,
+        getallusuarios,getusuario,create_usuario,verificacion_correo,alias_usuario,
 };
--- mi-backend/index.js
+++ mi-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');//se imoporta express
 const app = express();//aca tengo mi aplicacion
 const cors = require('cors'); 
 const port = 3000; //puerto donde va a correr mi aplicacion
-const {getallusuarios,getusuario} = require ('./dbase/usuarios');//importa todas las funcionesque estanen esa ruta
+const {getallusuarios,getusuario,create_usuario,verificacion_correo,alias_usuario} = require ('./dbase/usuarios');//importa todas las funcionesque estanen esa ruta
 app.use(express.json());
 app.use(cors()); // <--- CORS reubicado aquí
 
@@ -31,6 +31,59 @@ app.get('/usuario/:id',async (req, res) => {
         }catch(err){
                 res.sendStatus(404);
         }
+});
+//ruta para crear un usuario
+app.post('/usuario', async (req, res) => {
+
+  // 1️⃣ Validar body
+  if (req.body === undefined) {
+    return res.status(400).send("no se ha proveído el cuerpo");
+  }
+
+  const nombre = req.body.nombre;
+  const correo = req.body.correo;
+  const contraseña = req.body.contraseña;
+  const alias = req.body.alias;
+  const carta_favorita = req.body.carta_favorita;
+
+  // 2️⃣ Validaciones obligatorias
+  if (nombre === undefined) {
+    return res.status(400).send("no se ha proveído el nombre");
+  }
+
+  if (correo === undefined) {
+    return res.status(400).send("no se ha proveído el correo");
+  }
+
+  if (contraseña === undefined) {
+    return res.status(400).send("no se ha proveído la contraseña");
+  }
+
+  if (alias === undefined) {
+    return res.status(400).send("no se ha proveído el alias");
+  }
+
+  try {
+    // 3️⃣ Verificar si ya existe el correo
+    const existe_correo=await verificacion_correo(correo);
+    const usuario=await alias_usuario(alias);
+    if (existe_correo.rowCount !== 0) {
+      return res.status(409).send("ya existe un usuario con ese correo");
+    }
+     if (usuario.rowCount !== 0) {
+      return res.status(409).send("ya existe un usuario con ese alias");
+    }
+
+    const crear_usuario=await create_usuario(nombre, correo, contraseña, alias, carta_favorita);
+    if(crear_usuario===undefined){
+        return res.status(500).send("no se pudo crear el usuario");
+    }
+    res.status(201).json(crear_usuario);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("error interno del servidor");
+  }
 });
 
 const PORT = process.env.PORT || 3000;
