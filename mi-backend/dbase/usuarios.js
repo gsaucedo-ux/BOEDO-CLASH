c449806167bf5f54ad41a47b653042d96fafc092
gsaucedo-ux <gsaucedo@fi.uba.ar>
2026-01-25 00:20:22 +0000

    se agrega endpoint put y funciones de consulta para la base de datos

--- mi-backend/dbase/usuarios.js
+++ mi-backend/dbase/usuarios.js
@@ -27,10 +27,31 @@ async function verificacion_correo(correo) {
         const respuesta=await dbclient.query('SELECT * FROM usuario WHERE correo=$1', [correo]);
         return respuesta;
 }
+async function verificacion_correo_otros(correo, id_usuario) {
+  return dbclient.query(
+    'SELECT 1 FROM usuario WHERE correo = $1 AND id <> $2',
+    [correo, id_usuario]
+  );
+}
+async function alias_usuario_otros(alias, id_usuario) {
+  return dbclient.query(
+    'SELECT 1 FROM usuario WHERE alias = $1 AND id <> $2',
+    [alias, id_usuario]
+  );
+}
 async function alias_usuario(alias) {
         const respuesta=await dbclient.query('SELECT * FROM usuario WHERE alias=$1', [alias]);
         return respuesta;
 }
+
+async function update_usuario(id_usuario, nombre, correo, contraseña, alias, carta_favorita){
+        const respuesta=await dbclient.query('UPDATE usuario SET nombre=$1, correo=$2, contraseña=$3, alias=$4, carta_favorita=$5 WHERE id=$6 RETURNING *', [nombre, correo, contraseña, alias, carta_favorita,id_usuario]);
+        if (respuesta.rowCount===0){
+                return undefined;
+        }
+        return respuesta.rows[0];
+}
+
 module.exports={
-        getallusuarios,getusuario,create_usuario,verificacion_correo,alias_usuario,
+        getallusuarios,getusuario,create_usuario,update_usuario,verificacion_correo,verificacion_correo_otros,alias_usuario,alias_usuario_otros,
 };
--- mi-backend/index.js
+++ mi-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');//se imoporta express
 const app = express();//aca tengo mi aplicacion
 const cors = require('cors'); 
 const port = 3000; //puerto donde va a correr mi aplicacion
-const {getallusuarios,getusuario,create_usuario,verificacion_correo,alias_usuario} = require ('./dbase/usuarios');//importa todas las funcionesque estanen esa ruta
+const {getallusuarios,getusuario,create_usuario,update_usuario,verificacion_correo,verificacion_correo_otros,alias_usuario,alias_usuario_otros} = require ('./dbase/usuarios');//importa todas las funcionesque estanen esa ruta
 app.use(express.json());
 app.use(cors()); // <--- CORS reubicado aquí
 
@@ -62,11 +62,13 @@ app.post('/usuario', async (req, res) => {
   if (alias === undefined) {
     return res.status(400).send("no se ha proveído el alias");
   }
-
+if (carta_favorita === undefined) {
+    return res.status(400).send("no se ha proveído la carta favorita");
+  }
   try {
     // 3️⃣ Verificar si ya existe el correo
     const existe_correo=await verificacion_correo(correo);
-    const usuario=await alias_usuario(alias);
+    const usuario=await alias_usuario_otros(alias,id_usuario);
     if (existe_correo.rowCount !== 0) {
       return res.status(409).send("ya existe un usuario con ese correo");
     }
@@ -85,7 +87,65 @@ app.post('/usuario', async (req, res) => {
     res.status(500).send("error interno del servidor");
   }
 });
+//ruta para actualizar datos de usuario
+app.put('/usuarios/:id', async(req, res) => {
+        const id_usuario=req.params.id;
+        let usuario=await getusuario(id_usuario);
+        if(usuario==undefined){
+         return res.sendStatus(404);
+         };
+// 1️⃣ Validar body
+  if (req.body === undefined) {
+    return res.status(400).send("no se ha proveído el cuerpo");
+  }
 
+  const nombre = req.body.nombre;
+  const correo = req.body.correo;
+  const contraseña = req.body.contraseña;
+  const alias = req.body.alias;
+  const carta_favorita = req.body.carta_favorita;
+
+  // 2️⃣ Validaciones obligatorias
+  if (nombre === undefined) {
+    return res.status(400).send("no se ha proveído el nombre");
+  }
+
+  if (correo === undefined) {
+    return res.status(400).send("no se ha proveído el correo");
+  }
+
+  if (contraseña === undefined) {
+    return res.status(400).send("no se ha proveído la contraseña");
+  }
+
+  if (alias === undefined) {
+    return res.status(400).send("no se ha proveído el alias");
+  }
+if (carta_favorita === undefined) {
+    return res.status(400).send("no se ha proveído la carta favorita");
+  }
+  try {
+    // 3️⃣ Verificar si ya existe el correo
+    const existe_correo=await verificacion_correo_otros(correo, id_usuario);
+    const usuario=await alias_usuario(alias);
+    if (existe_correo.rowCount !== 0) {
+      return res.status(409).send("ya existe un usuario con ese correo");
+    }
+     if (usuario.rowCount !== 0) {
+      return res.status(409).send("ya existe un usuario con ese alias");
+    }
+
+    const actualizar_usuario=await update_usuario(id_usuario, nombre, correo, contraseña, alias, carta_favorita);
+    if(actualizar_usuario===undefined){
+        return res.status(500).send("no se pudo actualizarel usuario");
+    }
+    res.status(200).json(actualizar_usuario);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("error interno del servidor");
+  }
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor corriendo en http://localhost:" + PORT);
